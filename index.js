085c63cb07528b05270e34450c70cf8bfc4a4988
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-08-24 16:05:42 +0300

    Refactor the API

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const cors = require('cors');
+const config = require('./config');
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({
+        info: 'Node.js Movie Store web API'
+    })
+});
+app.listen(config.port, () => 
+    console.log(`Example app listening on port ${config.port}!`)
+);
