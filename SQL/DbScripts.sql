573fcab4dda444b01f2da7125109df4438e73972
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-08-24 15:52:20 +0300

    Added Record to Db

--- SQL/DbScripts.sql
+++ SQL/DbScripts.sql
@@ -4,3 +4,11 @@ CREATE TABLE movies(
    cover VARCHAR,
    created_on TIMESTAMP
 );
+
+INSERT INTO movies (
+	title, cover ) VALUES 
+	('3 Idiots', 'http://tr.web.img4.acsta.net/c_215_290/pictures/bzp/03/176535.jpg' )
+	
+INSERT INTO movies (
+	title, cover ) VALUES 
+	('Hitman', 'http://tr.web.img4.acsta.net/c_215_290/pictures/bzp/03/176535.jpg' )
